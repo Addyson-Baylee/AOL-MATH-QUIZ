564f09c70386bca03313634306f649c42e0aef28
Unknoown69420 <168260381+Unknoown69420@users.noreply.github.com>
2026-01-02 20:21:19 +0700

    Add question generation for multiple difficulty levels

--- AOL_Algo_Prog.c
+++ AOL_Algo_Prog.c
@@ -5,6 +5,27 @@
 void generateQuestion(int level, int *a, int *b, char *op, int *correctAnswer) {
     switch(level) {
 
+case 1: // Level Mudah (penjumlahan & pengurangan)
+            *a = rand() % 10 + 1;
+            *b = rand() % 10 + 1;
+            *op = (rand() % 2 == 0) ? '+' : '-';
+            *correctAnswer = (*op == '+') ? (*a + *b) : (*a - *b);
+            break;
+case 2: // Level Sedang (perkalian & pembagian)
+            *a = rand() % 20 + 1;
+            *b = rand() % 20 + 1;
+            *op = (rand() % 2 == 0) ? '*' : '/';
+            if (*op == '*') {
+                *correctAnswer = (*a * *b);
+            } else {
+                // Pastikan pembagian habis
+                *correctAnswer = rand() % 10 + 1;
+                *b = rand() % 10 + 1;
+                *a = *correctAnswer * *b;
+            }
+            break;
+
+
 
 int main (){
 
